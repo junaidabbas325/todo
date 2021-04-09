015dd1d8d9c5f3320fb6bd8cf58c661fc3634e72
junaidabbas325 <junaidabbas325@gmail.com>
2021-04-10 02:47:45 +0500

    working

--- src/screens/EditPost.js
+++ src/screens/EditPost.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { axiosInstance } from '../API/authorizationBearer'
-import './createPost.css'
+import './styles/CreatePost.css'
 
 
 const EditPost = (props) => {
